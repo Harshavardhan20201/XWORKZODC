e988ac6c1940a145c449ded4566e97d83a12f2e0
harsha200005@gmail.com <harsha200005@gmail.com>
2023-06-29 13:06:42 +0530

    adding second file

---
+++ Tool.java
@@ -0,0 +1,26 @@
+
+class Tool
+{
+    Tool()
+    {
+    System.out.println("The tools identies are");
+    }
+}
+
+class Tools
+{
+    public static void main(String[] args)
+    {
+        Tool hammer = new Tool();
+        Tool lonng = new Tool();
+        Tool nife = new Tool();
+        Tool stone = new Tool();
+        Tool arm = new Tool();
+        Tool sersor = new Tool();
+        Tool blade = new Tool();
+        Tool spaner = new Tool();
+        Tool scroodriver = new Tool();
+        Tool bolt = new Tool();
+
+    }
+}
