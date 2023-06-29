5368886fc9ae9179e3ddf55ab25477284b45a3fa
harsha200005@gmail.com <harsha200005@gmail.com>
2023-06-29 13:01:30 +0530

    adding file

---
+++ Movie.java
@@ -0,0 +1,27 @@
+class Movie
+{
+    Movie()
+    {
+        System.out.println("The movies are");
+    }
+}
+
+class Movies
+{
+    public static void main(String []args)
+    {
+        Movie gaja= new Movie();
+         Movie galipata= new Movie();
+         Movie ebandhana= new Movie();
+         Movie worldwaar= new Movie();
+         Movie kamjuring= new Movie();
+         Movie love= new Movie();
+          Movie kiss= new Movie();
+         Movie madhagaja= new Movie();
+         Movie happy= new Movie();
+          Movie dhiya= new Movie();
+         Movie villon= new Movie();
+         Movie socialnetwork= new Movie();
+
+    }
+}
