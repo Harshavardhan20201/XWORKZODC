029e64cbdc1e76edddd0814ec4196ca7d049e4c9
harsha200005@gmail.com <harsha200005@gmail.com>
2023-07-03 12:25:16 +0530

    adding new files

---
+++ Cricket.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class Cricket {
+    public static void main(String[] args) {
+        String players[] = {"virat", "Rohith", "Sachin", "Yuzi", "Faf", "Glenn"};
+        String Chocolate[] = {"cadbury", "dairy mil", "daily milk", "nembehuli", "orange choco", "kaccha mango bite", "dark"};
+        long Aadhar[] = {1045_8958_7845L, 8792_1457_8965L, 1445_5895_5412L, 4118_9745_821L, 1789_6587_52110000000L, 925878541254L};
+        for (int index = 0; index < 1; index++) {
+            System.out.println(Arrays.toString(players));
+            System.out.println(Arrays.toString(Chocolate));
+            System.out.println(Arrays.toString(Aadhar));
+        }
+    }
+}
