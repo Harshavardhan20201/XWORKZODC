57ad6079b19e6d1aa708818cc5cd792be66aee8d
HARSHAVARDHANA M <79176780+Harshavardhan20201@users.noreply.github.com>
2023-07-06 11:47:36 +0530

    Create MetroRunner.java

---
+++ MetroRunner.java
@@ -0,0 +1,7 @@
+public class MetroRunner{
+  public static void main(String args){
+    Metro runner1 = new Metro();
+    Metro runner2 = new Metro("Purple",500,"MG Road");
+    Metro runner3 = new Metro("Green", 500 , "Banashankri","Yeshwantpur",12)
+  }
+}
